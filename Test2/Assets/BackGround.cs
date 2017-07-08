881e4c2408c38b9618d2d3a4d2de51dff27d690f
sam <kvivaldi@gmail.com>
2017-07-08 09:28:27 +0900

    first commit

---
+++ Test2/Assets/BackGround.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGround : MonoBehaviour {
+
+
+    public float speed = 4.0f;
+	
+	// Update is called once per frame
+	void Update () {
+
+
+       // transform.position += Vector3.back * Time.deltaTime * speed;
+       // Mathf.Repeat(-Time.time, transform.localScale.z);
+
+        //transform.position = new Vector3(Mathf.Repeat(-Time.time, transform.localScale.z), transform.position.y, transform.position.z);
+ 
+        transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.Repeat(-Time.time * speed, transform.localScale.z));
+
+
+    }
+}
---
+++ Test2/Assets/NewBehaviourScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+    public float speed = 0.001F;
+    public float rotationSpeed = 100.0F;
+    void Update()
+    {
+        float z = Input.GetAxis("Vertical");
+        float x = Input.GetAxis("Horizontal");
+        transform.position = new Vector3(transform.position.x + x * speed, -1.0f, transform.position.z + z * speed);
+
+        Debug.Log("aaa:" + x + ":" + z);
+    }
+
+}
